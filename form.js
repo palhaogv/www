f61e820f6ba81955d2c6f6b1c9d6485079b7b2ef
palhaogv <palhaogv@gmail.com>
2021-10-11 16:35:51 +0200

    updating the javascript logging the data on the console

--- form.js
+++ form.js
@@ -1,14 +1,31 @@
+var fname = document.getElementById('fname-input')
+var lname = document.getElementById('lname-input')
+var number = document.getElementById('number-input')
+var email = document.getElementById('email-input')
+var message = document.getElementById('message-input')
+
+
+
 var modal = document.getElementById("modal");
 var btn = document.getElementById("popup-btn");
-var closeButton = document.getElementById("find-out");
-btn.onclick = function() {
+var closeBtn = document.getElementById("find-out");
+
+
+
+
+btn.onclick = function(event) { //function to 'open' de modal.
+    event.preventDefault()
+    typeof fname.value == 'string' ? console.log(fname.value) : NaN
+    typeof lname.value == 'string' ? console.log(lname.value) : NaN
+    email.value.includes('@') == true ? console.log(email.value) : NaN
+    typeof message.value == 'string' ? console.log(lname.value) : NaN
     modal.style.display = "flex";
 }
 
-closeButton.onclick = function() {
+closeBtn.onclick = function() { //function to 'close' de modal.
     modal.style.display = "none";
 }
-window.onclick = function(event) {
+window.onclick = function(event) { //function to 'close' de modal if you click outside of the div.
 if (event.target == modal) {
     modal.style.display = "none";
 }
